897d6c3770c4cbb57bc323c13df5e5a2e7b1ad1a
Romain Tello <romain.tello@gmail.com>
2020-12-01 11:46:21 +0100

    Beautify interface display

--- src/main/java/tba/Game/Game.java
+++ src/main/java/tba/Game/Game.java
@@ -1,11 +1,13 @@
 package tba.Game;
 
+import java.io.Console;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 import tba.Model.Direction;
 import tba.Model.Room;
 import tba.Model.RoomTransition;
+import tba.Utils.ConsoleColor;
 
 public class Game {
 
@@ -36,16 +38,17 @@ public class Game {
     {
         // Décrit la pièce dans laquelle se trouve le joueur actuellement
         Room currentRoom = state.getCurrentRoom();
-        System.out.println("You are in the " + currentRoom.getName() + ".\n");
-        System.out.println(currentRoom.getDescription());
+        System.out.println(ConsoleColor.CYAN + "You are in the " + currentRoom.getName() + ".\n" + ConsoleColor.RESET);
+        System.out.println(ConsoleColor.CYAN + currentRoom.getDescription() + ConsoleColor.RESET);
 
         for (RoomTransition transition: RoomTransition.findAllFromRoom(currentRoom)) {
-            System.out.println(transition.getDirection().getName() + " is the " + transition.getToRoom().getName() + ".");
+            System.out.println(ConsoleColor.GREEN + transition.getDirection().getName() + " is the " + transition.getToRoom().getName() + "." + ConsoleColor.RESET);
         }
 
         // Demande à l'utilisateur de saisir une commande
+        System.out.println("");
+        System.out.print("> ");
         String userInput = scanner.nextLine().trim().toLowerCase();
-        System.out.println(userInput);
 
         // Cherche si la saisie de l'utilisateur correspond à une commande de direction
         for (Direction direction: Direction.findAll()) {
@@ -57,7 +60,7 @@ public class Game {
 
                 // S'il n'existe pas de transition partant de la pièce actuelle dans la direction
                 if (transition == null) {
-                    System.out.println("You cannot go into that direction!");
+                    System.out.println(ConsoleColor.YELLOW + "You cannot go into that direction!" + ConsoleColor.RESET);
                 // Sinon, modifie la pièce actuelle
                 } else {
                     state.setCurrentRoom( transition.getToRoom() );
