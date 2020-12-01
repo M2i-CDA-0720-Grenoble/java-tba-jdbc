c6d1d438e2c29a922724eb2cecacab4cd7c54bbe
Romain Tello <romain.tello@gmail.com>
2020-12-01 12:11:12 +0100

    Clean up unnecessary imports

--- src/main/java/tba/App.java
+++ src/main/java/tba/App.java
@@ -1,12 +1,6 @@
 package tba;
 
-import java.util.List;
-
 import tba.Game.Game;
-import tba.Model.Direction;
-import tba.Model.Room;
-import tba.Model.RoomTransition;
-import tba.Utils.DatabaseHandler;
 
 /**
  * Hello world!
