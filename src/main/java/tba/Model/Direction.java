305765721d028eb2a66a75d482543b1c0f26db00
Romain Tello <romain.tello@gmail.com>
2020-12-01 09:37:27 +0100

    Add Direction model

--- src/main/java/tba/Model/Direction.java
+++ src/main/java/tba/Model/Direction.java
@@ -1,54 +1,49 @@
 package tba.Model;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import tba.Utils.DatabaseHandler;
 
-public class Room
-{
-
+public class Direction {
+    
     private int id;
     private String name;
-    private String description;
+    private String command;
 
-
-    public Room(int id, String name, String description)
+    public Direction(int id, String name, String command)
     {
         this.id = id;
         this.name = name;
-        this.description = description;
+        this.command = command;
     }
 
-    public static List<Room> findAll() throws SQLException
+    public static List<Direction> findAll() throws SQLException
     {
-        // Crée une liste prête à accueillir les objets qui vont être créés à partir des données récupérées
-        List<Room> rooms = new ArrayList<>();
+        List<Direction> directions = new ArrayList<>();
         // Envoie une requête en base de données et récupère les résultats
-        ResultSet set = DatabaseHandler.query("SELECT * FROM `room`");
+        ResultSet set = DatabaseHandler.query("SELECT * FROM `direction`");
         // Tant qu'il reste des résultats non traités, prend le résultat suivant...
         while (set.next()) {
             // ... et crée un objet à partir des colonnes présentes dans ce résultat
-            Room room = new Room(
+            Direction direction = new Direction(
                 set.getInt("id"),
                 set.getString("name"),
-                set.getString("description")
+                set.getString("command")
             );
             // Ajoute l'objet à la liste
-            rooms.add(room);
+            directions.add(direction);
         }
         // Renvoie la liste
-        return rooms;
+        return directions;
     }
 
-    public static Room findById(int id) throws SQLException
+    public static Direction findById(int id) throws SQLException
     {
         // Envoie une requête en base de données
         DatabaseHandler dbHandler = DatabaseHandler.getInstance();
-        PreparedStatement statement = dbHandler.getConnection().prepareStatement("SELECT * FROM `room` WHERE `id` = ?"
+        PreparedStatement statement = dbHandler.getConnection().prepareStatement("SELECT * FROM `direction` WHERE `id` = ?"
             // Rajouter ces deux lignes si on rencontre une erreur de type "Operation not allowed for a result set of type ResultSet.TYPE_FORWARD_ONLY"
             // ,ResultSet.TYPE_SCROLL_SENSITIVE
             // ,ResultSet.CONCUR_UPDATABLE
@@ -60,10 +55,10 @@ public class Room
         // ou aucun (s'il n'existe pas), cherche le premier résultat de la requête...
         if (set.first()) {
             // ...et renvoie un nouvel objet à partir de ses données
-            return new Room(
+            return new Direction(
                 set.getInt("id"),
                 set.getString("name"),
-                set.getString("description")
+                set.getString("command")
             );
         // Si la requête ne renvoie aucun résultat, renvoie null
         } else {
@@ -74,7 +69,7 @@ public class Room
     @Override
     public String toString()
     {
-        return "[Room #" + id + "] { name: " + name + ", description: " + description +" }";
+        return "[Direction #" + id + "] { name: " + name + ", command: " + command + " }";
     }
 
     public int getId() {
@@ -89,12 +84,12 @@ public class Room
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
+    public String getCommand() {
+        return command;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setCommand(String command) {
+        this.command = command;
     }
 
 }
