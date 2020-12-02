779d147bc41b562800715fb3f72213278a4fbef2
Romain Tello <romain.tello@gmail.com>
2020-12-02 11:08:38 +0100

    Add room CRUD

--- src/main/java/tba/Editor/EditorMode/RoomEditMode.java
+++ src/main/java/tba/Editor/EditorMode/RoomEditMode.java
@@ -23,7 +23,35 @@ public class RoomEditMode extends EditorMode {
 
     @Override
     public void interpret(int choice) {
+        Room room;
+        if (choice == 0) {
+            room = new Room();
+            Console.colorPrint("Editing new room", ConsoleColor.MAGENTA);
+        } else {
+            room = Room.findById(choice);
+            Console.colorPrint("Editing '" + room.getName() + "'", ConsoleColor.MAGENTA);
 
+            Console.warn("Do you want to delete '" + room.getName() + "'? (type YES to delete)");
+            String input = Console.input().toUpperCase();
+            if ("YES".equals(input)) {
+                room.delete();
+                return;
+            }
+        }
+
+        Console.colorPrint("Room name? [" + room.getName() + "]", ConsoleColor.GREEN_BRIGHT);
+        String name = Console.input();
+        if (!"".equals(name)) {
+            room.setName(name);
+        }
+
+        Console.colorPrint("Room description? [" + room.getDescription() + "]", ConsoleColor.GREEN_BRIGHT);
+        String description = Console.input();
+        if (!"".equals(description)) {
+            room.setDescription(description);
+        }
+
+        room.save();
     }
     
 }
